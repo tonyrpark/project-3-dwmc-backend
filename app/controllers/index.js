142caa6ec574f7cbfcedb0fcbd5da567a2cc1865
tonyrpark <tonyrpark@gmail.com>
2021-05-25 15:02:00 -0700

    stable version before pushing to heroku

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-const userCont = require('./user.controller');
-const walletCont = require('./wallet.controller');
+const userCont = require("./user.controller");
+const walletCont = require("./wallet.controller");
 module.exports = {
-    userCont,
-    walletCont
-=======
-const userCont = require('./user.controller');
-const walletCont = require('./wallet.controller');
-module.exports = {
-    userCont,
-    walletCont
->>>>>>> 3dd159bd1dac7483a22fe7d5ff9796751335d4b8
+  userCont,
+  walletCont,
 };
